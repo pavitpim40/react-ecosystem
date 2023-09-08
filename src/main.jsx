11cd67509df7fae01536c21d7294710b878ceedd
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 13:54:55 +0700

    setup route and page

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import axios from 'axios';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<div>Vite React Template</div>);
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+
+function HomePage() {
+  const [friend, setFriend] = useState([]);
+
+  const fetchFriend = async () => {
+    try {
+      const response = await axios.get('/users');
+      console.log(response.data);
+      setFriend(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchFriend();
+  }, []);
+  return (
+    <div className='App'>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+function ProfilePage() {
+  return <div className='App'>Profile Page</div>;
+}
+
+function FriendPage() {
+  return <div className='App'>Friend Page</div>;
+}
+
+function FeedPage() {
+  return <div className='App'>Feed Page</div>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Link to='/'>home</Link>
+    <Link to='/profile'>profile</Link>
+    <Link to='/profile/5'>friend</Link>
+    <Link to='/feed'>feed</Link>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      <Route path='/profile/:id' element={<FriendPage />} />
+      <Route path='/feed' element={<FeedPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+// npm i react-router-dom
+// npm i axios
