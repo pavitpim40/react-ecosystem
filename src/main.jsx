48f1d4085851a3d5fa05191ecf76a8307ab0dc2f
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 10:45:54 +0700

    finish:theme app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// Context
+// 1. createContext [Provider,Consumer] => ชื่อ Context
+const ThemeContext = createContext();
 
-function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+//
+// #### A1. สร้าง HOC : Higher Order Component [Provider]
+// HOC คือ FC ที่รับ Component เข้าไปและ return Component ใหม่ออกมา
+
+// function ThemeContextProvider(props) {
+//   console.log(props);
+//   return <div>{props.children}</div>;
+// }
+// function ThemeContextProvider(props) {
+//   console.log(props);
+//   return <ThemeContext.Provider>{props.children}</ThemeContext.Provider>;
+// }
+
+/*
+#### A2. Share Data & Logic ผ่าร arttribute value
+==> Data (state,boolean,string,object,array etc.)
+==> Logic (Fn ที่ใช้ handle ต่างๆ)
+*/
+
+// Data : isDarkMode,stylesOBj
+// Logic : setIsDarkMode,handleToggleTheme
+function ThemeContextProvider(props) {
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const stylesObj = {
     backgroundColor: isDarkMode ? 'black' : 'white',
@@ -13,12 +37,44 @@ function App() {
   const handleToggleTheme = () => {
     setIsDarkMode(!isDarkMode);
   };
+  const sharedObj = { theme: stylesObj, toggleTheme: handleToggleTheme };
+  return <ThemeContext.Provider value={sharedObj}>{props.children}</ThemeContext.Provider>;
+}
+
+/* 
+#### A3 นำ​ Provider ไปครอบ Children [Provider]
+ <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
+*/
+
+// ##################################################
+// ##################################################
+// ##################################################
+// ##################################################
+
+/*
+#### B1 : @Children Component ดึงค่า Shared object ผ่านตัว useContext
+SYNTAX : useContext(ContextName)
+ex. 
+const sharedObj = useContext(ThemeContext)
+*/
+
+// UI : Component
+function App() {
+  const s = useContext(ThemeContext);
+  console.log(s);
+
   return (
-    <div className='App' style={stylesObj}>
+    <div className='App' style={s.theme}>
       <h1>Theme App</h1>
-      <button onClick={handleToggleTheme}>Toggle Theme</button>
+      <button onClick={s.toggleTheme}>Toggle Theme</button>
     </div>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeContextProvider>
+    <App />
+  </ThemeContextProvider>
+);
