1157b94cc17d84714de7ac84d7095d5d62719c80
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 16:51:04 +0700

    button

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Button from '@mui/material/Button';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<div>Vite React Template</div>);
+function App() {
+  return (
+    <div>
+      <Button variant='text'>button</Button>
+      <Button variant='contained'>button</Button>
+      <Button variant='contained' disabled>
+        button
+      </Button>
+      <Button variant='outlined'>button</Button>
+
+      <Button variant='contained' color='primary'>
+        button
+      </Button>
+      <Button variant='contained' color='secondary'>
+        button
+      </Button>
+      <Button variant='contained' color='success'>
+        button
+      </Button>
+      <Button variant='contained' color='error' onClick={() => alert('Clicked')}>
+        button
+      </Button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
