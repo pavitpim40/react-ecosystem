544f8e092482cdd3fdda4bc6e8b29935f1b661de
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 11:49:38 +0700

    implement:isLoading with fakeAPI

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // สร้าง <AuthContext/> สำหรับ Provide isAuth,handleAuth ให้ <App/>
@@ -11,18 +11,46 @@ const AuthContext = createContext();
 
 function AuthContextProvider({ children }) {
   const [isAuth, setIsAuth] = useState(true);
-  const handleAuth = () => setIsAuth(!isAuth);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // isAuth : false => true ให้ deley 1 วิ
+  // useEffect(() => {
+  //   if (isLoading) {
+  //     setTimeout(() => {
+  //       setIsLoading(false);
+  //     }, 3000);
+  //   }
+  // }, [isLoading]);
+
+  const handleAuth = () => {
+    // login : isAuth : false => true
+    if (!isAuth) {
+      setIsLoading(true);
+      setTimeout(() => {
+        setIsAuth(true);
+        setIsLoading(false);
+      }, 3000);
+    } else {
+      setIsAuth(false);
+    }
+  };
 
   // const shareObj = { isAuth, handleAuth };
-  return <AuthContext.Provider value={{ isAuth, handleAuth }}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={{ isAuth, handleAuth, isLoading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 function App() {
-  const { isAuth, handleAuth } = useContext(AuthContext);
+  const { isAuth, handleAuth, isLoading } = useContext(AuthContext);
   return (
     <div className='App'>
-      <h1>Welcome.. {!isAuth ? 'Guest' : 'User'} </h1>
-      <button onClick={handleAuth}>{!isAuth ? 'Login' : 'Logout'}</button>
+      {isLoading ? <h1>Loding...</h1> : <h1>Welcome.. {!isAuth ? 'Guest' : 'User'} </h1>}
+      <button onClick={handleAuth} disabled={isLoading}>
+        {!isAuth ? 'Login' : 'Logout'}
+      </button>
     </div>
   );
 }
