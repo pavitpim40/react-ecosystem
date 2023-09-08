78247c5c58dc5411ac0db32493ca2b0a4c449fd8
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 09:59:50 +0700

    handle with state

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<div>Vite React Template</div>);
+function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const stylesObj = {
+    backgroundColor: isDarkMode ? 'black' : 'white',
+    color: isDarkMode ? 'white' : 'black',
+  };
+
+  const handleToggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+  return (
+    <div className='App' style={stylesObj}>
+      <h1>Theme App</h1>
+      <button onClick={handleToggleTheme}>Toggle Theme</button>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
