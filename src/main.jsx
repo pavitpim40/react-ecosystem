e08fd1fe2fbba226261df325f2e1b0fd9b0b87f0
Pavit Pimchanagul <pavitpim@gmail.com>
2023-09-08 17:26:31 +0700

    mui

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import AppBar from './Appbar';
 import Button from '@mui/material/Button';
+import Card from './Card';
+import { orange } from '@mui/material/colors';
+
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+const theme = createTheme({
+  status: {
+    danger: orange[500],
+  },
+});
 
 function App() {
   return (
-    <div>
-      <Button variant='text'>button</Button>
-      <Button variant='contained'>button</Button>
-      <Button variant='contained' disabled>
-        button
-      </Button>
-      <Button variant='outlined'>button</Button>
-
-      <Button variant='contained' color='primary'>
-        button
-      </Button>
-      <Button variant='contained' color='secondary'>
-        button
-      </Button>
-      <Button variant='contained' color='success'>
-        button
-      </Button>
-      <Button
-        variant='contained'
-        color='error'
-        onClick={() => alert('Clicked')}
-        sx={{ padding: '10px', marginTop: '20px', borderRadius: '10px' }}
-      >
-        button
-      </Button>
-    </div>
+    <Box>
+      <AppBar />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={4} sx={{ padding: '20px' }}>
+          <Grid item xs={0} md={2}>
+            SideBar
+          </Grid>
+          <Grid item xs={12} md={8} sx={{ padding: '20px' }}>
+            <Card />
+          </Grid>
+          <Grid item xs={0} md={2}>
+            <Button status='danger'>Click</Button>
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
